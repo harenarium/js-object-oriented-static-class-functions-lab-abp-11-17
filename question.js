70cec58d194d2b0d2b2c71f34b7638f537a1254b
harenarium <carolinel2610@gmail.com>
2017-11-13 20:29:29 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,20 @@
 'use strict';
+
+class Questions{
+  constructor(content){
+    this.content = content
+    this.save()
+  }
+  static All(){
+    return this._All;
+  }
+  save(){
+    this.constructor._All.push(this)
+  }
+  find(id){
+    
+  }
+  
+}
+
+Questions._All = [];
